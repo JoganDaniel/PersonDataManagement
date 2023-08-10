b8f63531e3c7ed5a47ecd969407308fcf5342165
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-10 10:39:30 +0530

    [jogan] Person Class created and users added

---
+++ PersonDataManagement/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    public class Operations
+    {
+        public void RetrieveTopTwoRecords(List<Person> list)
+        {
+            var result = list.Where(x => x.Age < 60).Take(2);
+            Display(result.ToList());
+        }
+
+        public void RetrieveRecordsInBetweenAge(List<Person> list)
+        {
+            var resullt = list.Where(x => x.Age > 13 && x.Age < 18);
+            Display(resullt.ToList());
+        }
+        public void Display(List<Person> list)
+        {
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.SN+" "+data.Name+" "+data.Address+" "+data.Age);
+            }
+        }
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace PersonDataManagement
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person()
+            {
+                SN = 1,
+                Name = "a",
+                Address = "a",
+                Age = 15
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 2,
+                Name = "b",
+                Address = "b",
+                Age = 18
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 3,
+                Name = "c",
+                Address = "c",
+                Age = 21
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 4,
+                Name = "d",
+                Address = "d",
+                Age = 17
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 5,
+                Name = "e",
+                Address = "e",
+                Age = 25
+            });
+
+            persons.Add(new Person()
+            {
+                SN =6,
+                Name = "f",
+                Address = "f",
+                Age = 24
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 7,
+                Name = "g",
+                Address = "g",
+                Age = 40
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 8,
+                Name = "h",
+                Address = "h",
+                Age = 36
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 9,
+                Name = "i",
+                Address = "i",
+                Age = 72
+            });
+
+            persons.Add(new Person()
+            {
+                SN = 10,
+                Name = "j",
+                Address = "j",
+                Age = 22
+            });
+            Operations operation = new Operations();
+            operation.Display(persons);
+            Console.WriteLine();
+            operation.RetrieveTopTwoRecords(persons);
+            Console.WriteLine();
+            operation.RetrieveRecordsInBetweenAge(persons);
+        }
+    }
+}
